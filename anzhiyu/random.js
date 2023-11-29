f724653aa9426babba086ef28efc13b1aedf25c1
Johnny <1479456702@qq.com>
2023-11-29 14:14:08 +0800

    Site updated: 2023-11-29 14:14:07

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2023/11/27/20231127-vulhub-DC/","2023/11/29/20231129-OWASP-TOP10靶场/"];function toRandomPost(){
+var posts=["2023/11/27/20231127-vulhub-DC/","2023/11/29/20231129-OWASP-TOP10靶场/","2023/09/04/20231129-SEED-Labs-2.0/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
