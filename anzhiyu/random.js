7824256917409a6d310306f2235163ba7daf03a1
Johnny <1479456702@qq.com>
2024-01-12 21:40:19 +0800

    Site updated: 2024-01-12 21:40:18

---
+++ anzhiyu/random.js
@@ -0,0 +1,3 @@
+var posts=["2023/11/27/20231127-vulhub-DC/","2023/09/04/20231129-SEED-Labs-2.0/","2023/11/29/20231129-OWASP-TOP10靶场/","2023/01/02/20240102-网络空间安全技术/","2022/08/07/Eureka/","2022/02/07/Java基础学习笔记/","2024/01/11/Nginx内存管理/","2023/12/25/20231225-Golang-channel/","2022/08/08/OpenFeign/","2022/08/07/Ribbon/","2024/12/02/SCUCTF2023/"];function toRandomPost(){
+    pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
+  };
